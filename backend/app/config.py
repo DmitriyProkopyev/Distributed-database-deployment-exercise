771da484477b2366d518a7e899da1a52f034c391
Alena <astarikova@k2.cloud>
2025-04-29 23:13:55 +0300

    fix bug

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     MONGO_URI: str = "mongodb://localhost:27017"
-    MONGO_DB_NAME: str = "distributed_database"
+    MONGO_DB_NAME: str = "distributed_database_kafka"
     KAFKA_BOOTSTRAP_SERVERS: str = "kafka:9092"
     KAFKA_TOPIC_NGINX_TO_BACKEND: str = "nginx_requests"
     KAFKA_TOPIC_BACKEND_TO_DB: str = "db_commands"
